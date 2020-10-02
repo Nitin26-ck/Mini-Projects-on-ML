a9d5e57b00fbe3b2c8744f7a7fc7665651fdca19
Nitin C Kallappa <57488263+Nitin26-ck@users.noreply.github.com>
2020-10-02 17:16:17 +0530

    Update linear regression 1.py

--- Predicting the salary/linear regression 1.py
+++ Predicting the salary/linear regression 1.py
@@ -10,16 +10,13 @@ Created on Fri Jul 10 15:48:08 2020
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-dataset = pd.read_csv('E:/Machine Learning/P14-Part2-Regression/P14-Part2-Regression/Section 6 - Simple Linear Regression/Python/Salary_Data.csv')
+dataset = pd.read_csv('/Salary_Data.csv')
 
 x= dataset.iloc[:,:-1].values
 y= dataset.iloc[:, 1].values
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=1/3, random_state=0)
-"""from sklearn.preprocessing import StandardScaler
-sc_x = StandardScaler()
-x_train=sc_x.fit_transform(x_train)
-x_test=sc_x.fit_transform(x_test)"""
+
 
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
