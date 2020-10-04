99cd7d0e20e1daa91d5647324b6c9c9cf2be0d4a
Nitin C Kallappa <57488263+Nitin26-ck@users.noreply.github.com>
2020-10-04 23:15:58 +0530

    Add files via upload

---
+++ SAT vs GPA/simple linear rgrssion 2.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 11 00:04:00 2020
+
+@author: Nitin
+"""
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('E:/Machine Learning/P14-Part2-Regression/P14-Part2-Regression/Section 6 - Simple Linear Regression/Python/Linear_Regression.csv')
+
+x= dataset.iloc[:,:-1].values
+y= dataset.iloc[:, 1].values
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.4, random_state=0)
+"""from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train=sc_x.fit_transform(x_train)
+x_test=sc_x.fit_transform(x_test)"""
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train,y_train)
+
+y_pred=regressor.predict(x_test)
+
+plt.scatter(x_train,y_train,color='red')
+plt.plot(x_train,regressor.predict(x_train), color='blue')
+plt.title('SAT vs GPA')
+plt.xlabel('SAT')
+plt.ylabel('GPA')
+plt.show()
+
+plt.scatter(x_test,y_test,color='red')
+plt.plot(x_train,regressor.predict(x_train), color='blue')
+plt.title('SAT vs GPA')
+plt.xlabel('SAT')
+plt.ylabel('GPA')
+plt.show()
