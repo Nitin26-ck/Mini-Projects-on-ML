e330f8044b142d8297ebd8c625374fe99724f739
Nitin C Kallappa <nitinckallappa@gmail.com>
2020-10-12 13:32:59 +0530

    Add files via upload

---
+++ Predicting the salary based on position/4.Support Vector Regression (SVR)/SVR.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 13:09:21 2020
+
+@author: Nitin
+"""
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('E:/Machine Learning/P14-Part2-Regression/P14-Part2-Regression/Section 9 - Support Vector Regression (SVR)/Python/Position_Salaries.csv')
+x= dataset.iloc[:,1:-1].values
+y= dataset.iloc[:,-1].values
+
+y = y.reshape(len(y),1)
+
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+sc_y = StandardScaler()
+x = sc_x.fit_transform(x)
+y = sc_y.fit_transform(y)
+
+from sklearn.svm import SVR
+regressor = SVR(kernel='rbf')
+regressor.fit(x,y)
+
+y_pred = sc_y.inverse_transform(regressor.predict(sc_x.transform([[6.5]])))
+
+plt.scatter(sc_x.inverse_transform(x), sc_y.inverse_transform(y), color = 'red')
+plt.plot(sc_x.inverse_transform(x), sc_y.inverse_transform(regressor.predict(x)), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the SVR results (for higher resolution and smoother curve)
+"""x_grid = np.arange(min(sc_x.inverse_transform(x)), max(sc_x.inverse_transform(x)), 0.1)
+x_grid = x_grid.reshape((len(x_grid), 1))
+plt.scatter(sc_x.inverse_transform(x), sc_y.inverse_transform(y), color = 'red')
+plt.plot(x_grid, sc_y.inverse_transform(regressor.predict(sc_x.transform(x_grid))), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()"""
