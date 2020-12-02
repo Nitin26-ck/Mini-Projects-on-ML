af1192b18775e04401a67b83302f721d47a04892
Nitin C Kallappa <nitinckallappa@gmail.com>
2020-12-02 23:58:30 +0530

    Add files via upload

---
+++ Finding the wine Quality/Principal Component Analysis (PCA)/PCA.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  6 23:28:38 2020
+
+@author: Nitin
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('E:/Machine Learning/P14-Part9-Dimensionality-Reduction/P14-Part9-Dimensionality-Reduction/Section 38 - Principal Component Analysis (PCA)/Python/Wine.csv')
+x= dataset.iloc[:,0:13].values
+y= dataset.iloc[:, 13].values
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=1/3, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train=sc_x.fit_transform(x_train)
+x_test=sc_x.fit_transform(x_test)
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+x_train = pca.fit_transform(x_train)
+x_test = pca.transform(x_test)
+explained_variance = pca.explained_variance_ratio_
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(x_train,y_train)
+
+y_pred = classifier.predict(x_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+from matplotlib.colors import ListedColormap
+x_set,y_set = x_train,y_train
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green','blue')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green','blue'))(i), label = j)
+plt.title('Logistic_Regression(train)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.show()
+
+from matplotlib.colors import ListedColormap
+x_set,y_set = x_test,y_test
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green','blue')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green','blue'))(i), label = j)
+plt.title('Logistic_Regression(test)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.show()
