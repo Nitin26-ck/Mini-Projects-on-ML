bdc2811deb8b19691e2a6216b5be13628f05251d
Nitin C Kallappa <nitinckallappa@gmail.com>
2021-02-19 23:54:22 +0530

    Update linear regression 1.py

--- Predicting the salary/linear regression 1.py
+++ Predicting the salary/linear regression 1.py
@@ -6,7 +6,7 @@ Created on Fri Jul 10 15:48:08 2020
 """
 
 
-
+#importing modules and dataset
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,16 +14,19 @@ dataset = pd.read_csv('/Salary_Data.csv')
 
 x= dataset.iloc[:,:-1].values
 y= dataset.iloc[:, 1].values
+
+#data-preprocessing
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=1/3, random_state=0)
 
-
+#linear regression
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(x_train,y_train)
 
 y_pred=regressor.predict(x_test)
 
+#plot
 plt.scatter(x_train,y_train,color='red')
 plt.plot(x_train,regressor.predict(x_train), color='blue')
 plt.title('Salary vs experiance')
