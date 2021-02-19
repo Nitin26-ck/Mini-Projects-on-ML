526f0856f39a68efc1b75e0b8a600d5098b13630
Nitin C Kallappa <nitinckallappa@gmail.com>
2021-02-19 23:49:26 +0530

    Update simple linear rgrssion 2.py

--- SAT vs GPA/simple linear rgrssion 2.py
+++ SAT vs GPA/simple linear rgrssion 2.py
@@ -5,7 +5,7 @@ Created on Sat Jul 11 00:04:00 2020
 @author: Nitin
 """
 
-
+#importing modules and dataset
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,6 +13,8 @@ dataset = pd.read_csv('E:/Machine Learning/P14-Part2-Regression/P14-Part2-Regres
 
 x= dataset.iloc[:,:-1].values
 y= dataset.iloc[:, 1].values
+
+#data preprocessing
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.4, random_state=0)
 """from sklearn.preprocessing import StandardScaler
@@ -20,12 +22,14 @@ sc_x = StandardScaler()
 x_train=sc_x.fit_transform(x_train)
 x_test=sc_x.fit_transform(x_test)"""
 
+#linear regression
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(x_train,y_train)
 
 y_pred=regressor.predict(x_test)
 
+#plot
 plt.scatter(x_train,y_train,color='red')
 plt.plot(x_train,regressor.predict(x_train), color='blue')
 plt.title('SAT vs GPA')
